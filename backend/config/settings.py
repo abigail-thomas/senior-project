5b8ac831e3f0ab2ce7e64f4bad3be9939f89f672
Abigail Thomas <agthomas084@gmail.com>
2026-08-29 18:06:49 -0400

    Configure CORS for frontend

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
+    'locations',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,3 +127,5 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+CORS_ALLOWED__ORIGINS = ["http://localhost:5173", "http://127.0.0.1:5173",]
