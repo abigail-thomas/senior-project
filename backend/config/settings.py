647e8227430340889397bcbd238992748bdd6176
Abigail Thomas <agthomas084@gmail.com>
2026-08-31 18:49:43 -0400

    Debugging port

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -128,4 +128,6 @@ MAILERS = {
     },
 }
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:5173", "http://127.0.0.1:5173",]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173", "http://127.0.0.1:5173",
+    ]
